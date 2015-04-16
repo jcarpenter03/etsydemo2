dec81f2845fb3697888a9494d0e731132fdba366
Jay <james.carpentera3@gmail.com>
2015-04-16 22:16:32 +0000

    Fixed Dropbox Image Error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = "durpz9n4gn7i12f"
-  config.dropbox_app_secret = ENV["APP_SECRET"]
-  config.dropbox_access_token = "4d7vh681cve6qbu0"
-  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  config.dropbox_app_key = "yim7oob0z3ljr5q"
+  config.dropbox_app_secret = "iusxpmf2q62bgol"
+  config.dropbox_access_token = "44mqb3vy7jsrbguz"
+  config.dropbox_access_token_secret = "mz44evfifm4vurl"
   config.dropbox_user_id = "134513580"
   config.dropbox_access_type = "app_folder"
 end
